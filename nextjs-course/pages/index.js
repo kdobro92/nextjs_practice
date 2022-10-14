f137008b74ed334b4bd35f62437dae2df78c5e3f
kdobro92 <kdobro92@gmail.com>
2022-10-15 02:46:04 +0900

    mini_project

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/Link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfoliio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
