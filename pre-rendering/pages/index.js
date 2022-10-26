bae7120820eaa507cdf491ae343c3c35dba781ea
kdobro92 <kdobro92@gmail.com>
2022-10-26 18:26:13 +0900

    dynamic parameter

--- pre-rendering/pages/index.js
+++ pre-rendering/pages/index.js
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs/promises";
+import Link from "next/link";
 
 function HomePage(props) {
   const { products } = props;
@@ -7,25 +8,37 @@ function HomePage(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
-// how to use getStaticProps
-
 export async function getStaticProps() {
-  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
     revalidate: 10,
+    notFound: true,
   };
 }
 
