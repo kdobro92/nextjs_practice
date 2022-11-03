3cc2b88f94c72411461a65b7ec246d464e978315
kdobro92 <kdobro92@gmail.com>
2022-11-03 22:51:04 +0900

    getServerSideProps

--- pre-rendering/pages/user-profile.js
+++ pre-rendering/pages/user-profile.js
@@ -5,6 +5,11 @@ function UserProfilepage(props) {
 export default UserProfilepage;
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  console.log(req);
+  console.log(res);
+
   return {
     props: { username: "Max" },
   };
